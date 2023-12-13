23e98661c67eb2b1a4ef672821968f52e59ca1b1
AsimAbdullaPP <153506962+AsimAbdullaPP@users.noreply.github.com>
2023-12-13 17:29:36 +0530

    Update analyser.py

--- roop/analyser.py
+++ roop/analyser.py
@@ -1,5 +1,6 @@
 import insightface
 import roop.globals
+import roop.core
 
 FACE_ANALYSER = None
 
@@ -13,11 +14,24 @@ def get_face_analyser():
 
 
 def get_face_single(img_data):
-    face = get_face_analyser().get(img_data)
-    try:
-        return sorted(face, key=lambda x: x.bbox[0])[0]
-    except IndexError:
+    face_analyser = get_face_analyser()
+    faces = face_analyser.get(img_data)
+
+    selected_face_index = roop.core.get_selected_face_index()
+
+    if not faces:
         return None
+
+    if selected_face_index is not None:
+        try:
+            for index in selected_face_index:
+                return faces[index]
+        except IndexError:
+            print("Invalid face index. Using the first detected face.")
+            return faces[0]
+    else:
+        # Default behavior: Sort faces based on bounding box x-coordinate and choose the first one
+        return sorted(faces, key=lambda x: x.bbox[0])[0]
 
 
 def get_face_many(img_data):
